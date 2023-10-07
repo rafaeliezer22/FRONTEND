79f507f9c95332dc7776e69305dd7141e75069ba
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-10-07 10:24:51 -0300

    Acualizamos menu hamburgusa
    Acualizamos menu hamburgusa
    del navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,16 +1,15 @@
-// script.js
-// const buttons = document.querySelectorAll("button");
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
-// buttons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//         alert("Producto agregado al carrito");
-//     });
-// });
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
 
-
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("menu");
-
-menuToggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
+    // Cierra el menú cuando se hace clic en cualquier otro lugar de la página
+    document.addEventListener("click", function (event) {
+        if (!menu.contains(event.target) && event.target !== menuToggle) {
+            menu.classList.remove("active");
+        }
+    });
 });
