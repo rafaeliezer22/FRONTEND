bd5fffe4b3e78899d448d28dd1c0c8e080601814
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-10-07 11:18:53 -0300

    Reserva
    Se hicieron unoscambios en reserva.html
    se modifico el js, y el css
    y se crea un ventana modal
    tambien se simula un base da datos de canchas

--- scripts/reserva.js
+++ scripts/reserva.js
@@ -1,38 +1,84 @@
-// Supongamos que tienes un arreglo de canchas
-const canchas = [
-    { nombre: "Cancha 1", disponibilidad: true },
-    { nombre: "Cancha 2", disponibilidad: false },
-    // Agrega más canchas aquí
-];
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulamos la información de las canchas en un arreglo
+    const canchasData = [
+        { id: 1, nombre: "Cancha 1", precio: 20, disponible: true },
+        { id: 2, nombre: "Cancha 2", precio: 25, disponible: false },
+        { id: 3, nombre: "Cancha 3", precio: 15, disponible: true },
+        { id: 4, nombre: "Cancha 4", precio: 25, disponible: true },
+    ];
 
-const canchasContainer = document.getElementById("canchas");
+    const canchasContainer = document.querySelector(".canchas-container");
+    const reservaMessage = document.getElementById("reserva-message");
+    const modal = document.getElementById("modal");
+    const modalContent = document.getElementById("modal-content");
+    const confirmarBtn = document.getElementById("confirmar-btn");
+    const cancelarBtn = document.getElementById("cancelar-btn");
 
-// Función para mostrar la lista de canchas
-function mostrarCanchas() {
-    canchasContainer.innerHTML = "";
-    canchas.forEach((cancha, index) => {
-        const canchaDiv = document.createElement("div");
-        canchaDiv.classList.add("cancha");
-        canchaDiv.innerHTML = `
-            <h2>${cancha.nombre}</h2>
-            <p>Disponibilidad: ${cancha.disponibilidad ? 'Disponible' : 'No disponible'}</p>
-            <button class="reservar" onclick="reservarCancha(${index})">Reservar</button>
-        `;
-        canchasContainer.appendChild(canchaDiv);
-    });
-}
+    let reservaCancha = null; // Almacena la cancha reservada actualmente
 
-// Función para reservar una cancha
-function reservarCancha(index) {
-    if (canchas[index].disponibilidad) {
-        alert(`Has reservado la ${canchas[index].nombre}`);
-        // Aquí puedes agregar la lógica para actualizar la disponibilidad en la base de datos
-        canchas[index].disponibilidad = false;
-        mostrarCanchas(); // Actualiza la lista de canchas
-    } else {
-        alert(`La ${canchas[index].nombre} no está disponible.`);
+    // Función para mostrar las canchas en la página
+    function mostrarCanchas() {
+        canchasContainer.innerHTML = "";
+
+        canchasData.forEach(cancha => {
+            const canchaCard = document.createElement("div");
+            canchaCard.classList.add("cancha-card");
+
+            const estado = cancha.disponible ? "Disponible" : "No disponible";
+            canchaCard.innerHTML = `
+                <h2>${cancha.nombre}</h2>
+                <img src="https://images.vexels.com/media/users/3/146856/isolated/preview/d8bb6dc5ecf8241bd7e298e6883f715c-icono-de-campo-de-f-uacute-tbol-by-vexels.png" alt="Imagen de la cancha ${cancha.nombre}" width="200" height="150">
+                <p>Precio: $${cancha.precio} por hora</p>
+                <p>Estado: ${estado}</p>
+                <button class="reservar-btn" data-id="${cancha.id}">Reservar</button>
+            `;
+
+            canchaCard.querySelector(".reservar-btn").addEventListener("click", () => {
+                if (cancha.disponible) {
+                    reservaCancha = cancha;
+                    mostrarModal();
+                } else {
+                    alert(`Lo siento, la Cancha ${cancha.nombre} no está disponible.`);
+                }
+            });
+
+            canchasContainer.appendChild(canchaCard);
+        });
     }
-}
 
-// Mostrar la lista de canchas al cargar la página
-mostrarCanchas();
+    // Función para mostrar la ventana modal
+    function mostrarModal() {
+        modal.style.display = "block";
+        modalContent.innerHTML = `
+            <h2>Detalles de la Reserva</h2>
+            <p>Has reservado la Cancha ${reservaCancha.nombre} por $${reservaCancha.precio} por hora.</p>
+            <button id="confirmar-btn">Confirmar/Pagar</button>
+            <button id="cancelar-btn">Cancelar</button>
+        `;
+
+        // Agregar eventos a los botones de la modal
+        document.getElementById("confirmar-btn").addEventListener("click", confirmarReserva);
+        document.getElementById("cancelar-btn").addEventListener("click", cancelarReserva);
+    }
+
+    // Función para confirmar la reserva
+    function confirmarReserva() {
+        reservaCancha.disponible = false;
+        mostrarMensajeReserva(reservaCancha);
+        cerrarModal();
+    }
+
+    // Función para cancelar la reserva
+    function cancelarReserva() {
+        reservaCancha = null;
+        cerrarModal();
+    }
+
+    // Función para cerrar la ventana modal
+    function cerrarModal() {
+        modal.style.display = "none";
+    }
+
+    // Llamamos a la función para mostrar las canchas al cargar la página
+    mostrarCanchas();
+});
