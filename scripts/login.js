d8c3ebde99077f7858206ff6540251102db2bdcd
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-10-10 01:12:49 -0300

    Validacion de Registro
    - Se validaron lo registros
    - Se reescibio la funcion toggle para el cambio entre el login y registro
    - se cambio el fondo  y se actulizo algunos estilos

--- scripts/login.js
+++ scripts/login.js
@@ -1,31 +1,41 @@
-document.getElementById('login-form').addEventListener('submit', function (e) {
-    e.preventDefault();
+// document.getElementById('login-form').addEventListener('submit', function (e) {
+//     e.preventDefault();
 
     // Simula un inicio de sesión exitoso
-    alert('¡Inicio de sesión exitoso!');
+    // alert('¡Inicio de sesión exitoso!');
     // Aquí puedes redirigir al usuario a la página deseada después del inicio de sesión.
-});
+// });
 
-document.getElementById('register-form').addEventListener('submit', function (e) {
-    e.preventDefault();
+// document.getElementById('register-form').addEventListener('submit', function (e) {
+//     e.preventDefault();
 
     // Aquí puedes agregar la lógica de registro, por ejemplo, enviar datos al servidor.
-    alert('¡Registro exitoso!');
-});
+    // alert('¡Registro exitoso!');
+// });
+const loginContainer = document.querySelector('.login-container');
+const registerContainer = document.querySelector('.register-container');
+loginContainer.style.display = 'block';
+registerContainer.style.display = 'none';
+
 
 // Función para alternar entre los formularios de inicio de sesión y registro
-function toggleForm() {
-    const loginContainer = document.querySelector('.login-container');
-    const registerContainer = document.querySelector('.register-container');
+function toggleFormRegister() {
 
     if (loginContainer.style.display === 'block') {
         loginContainer.style.display = 'none';
         registerContainer.style.display = 'block';
-    } else {
+    }  
+}
+
+function toggleFormLogin() {
+    if (registerContainer.style.display === 'block'){
         loginContainer.style.display = 'block';
         registerContainer.style.display = 'none';
     }
 }
 
 // Evento para alternar entre los formularios al hacer clic en el enlace
-document.querySelector('.toggle-form-button').addEventListener('click', toggleForm);
+document.querySelector('.toggle-form-button-login').addEventListener('click', toggleFormLogin);
+document.querySelector('.toggle-form-button-register').addEventListener('click', toggleFormRegister);
+
+    
